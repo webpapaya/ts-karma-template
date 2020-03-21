112c1bacd3de0a154a3026ea8d79d5b2bbbda144
Gregor Riegler <gregor.riegler@ebcont.com>
2020-03-21 14:24:01 +0100

    Cell immutable

--- src/chai.spec.ts
+++ src/chai.spec.ts
@@ -1,10 +1,15 @@
 import  { expect as bla } from 'chai';
 
 class Cell {
-  private isAlive: boolean;
+  private readonly isAlive: boolean;
+
+  constructor(alive: boolean) {
+    this.isAlive = alive;
+  }
 
   evolveByLivingNeighborCount(neighbors: number) {
-    this.isAlive = neighbors === 3;
+    let alive = neighbors === 3;
+    return new Cell(alive);
   }
 
   alive() {
@@ -15,14 +20,14 @@ class Cell {
 describe("game of life mob", () => {
 
   it('cell without neighbors should die', () => {
-    let cell = new Cell();
-    cell.evolveByLivingNeighborCount(0);
-    expect(cell.alive()).toBeFalse();
+    let cell = new Cell(false);
+    let newCell = cell.evolveByLivingNeighborCount(0);
+    expect(newCell.alive()).toBeFalse();
   });
 
   it('dead cell with 3 neighbors comes alive', () => {
-    let cell = new Cell();
-    cell.evolveByLivingNeighborCount(3);
-    expect(cell.alive()).toBeTrue();
+    let cell = new Cell(false);
+    let newCell = cell.evolveByLivingNeighborCount(3);
+    expect(newCell.alive()).toBeTrue();
   })
 });
