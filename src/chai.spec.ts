9c884e2bfc24b11e6029bb233d288da19ada8c72
Görge Albrecht <goerge@code-mentor.com>
2020-03-21 14:07:28 +0100

    WIP

--- src/chai.spec.ts
+++ src/chai.spec.ts
@@ -1,11 +1,20 @@
 import  { expect as bla } from 'chai';
 
-describe("a testsuite with chai expections", () => {
+class Cell {
+  evolveByLivingNeighbourCount(neighbours: number) {
+
+  }
+
+  alive() : boolean{
+    return undefined;
+  }
+}
+
+describe("game of life mob", () => {
 
   it('cell without neighbors should die', () => {
-
     let cell = new Cell();
-    cell.evolveByLivingNeighbourCount()
+    cell.evolveByLivingNeighbourCount(0);
     expect(cell.alive()).toBeFalse()
   });
 
