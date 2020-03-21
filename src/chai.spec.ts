cfd6feb77f7d812b87fc6304cee1af3668e5d147
Gregor Riegler <gregor.riegler@ebcont.com>
2020-03-21 14:19:33 +0100

    dead cell with 3 neighbors comes alive

--- src/chai.spec.ts
+++ src/chai.spec.ts
@@ -17,12 +17,12 @@ describe("game of life mob", () => {
   it('cell without neighbors should die', () => {
     let cell = new Cell();
     cell.evolveByLivingNeighborCount(0);
-    expect(cell.alive()).toEqual(undefined);
+    expect(cell.alive()).toBeFalse();
   });
 
   it('dead cell with 3 neighbors comes alive', () => {
     let cell = new Cell();
-    cell.evolveByLivingNeighborCount(3)
-    expect(cell.alive()).toBeTrue()
+    cell.evolveByLivingNeighborCount(3);
+    expect(cell.alive()).toBeTrue();
   })
 });
