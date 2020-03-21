e2210a51a86bacc56f2d451cfd281e5d8245aa88
Gregor Riegler <gregor.riegler@ebcont.com>
2020-03-21 14:00:41 +0100

    wip

--- src/chai.spec.ts
+++ src/chai.spec.ts
@@ -2,9 +2,11 @@ import  { expect as bla } from 'chai';
 
 describe("a testsuite with chai expections", () => {
 
-  it('should execute this test', () => {
-    bla([1,23, 4]).to.be.ordered;
-    //expect(true).toBeTruthy();
+  it('cell without neighbors should die', () => {
+
+    let cell = new Cell();
+    cell.evolveByLivingNeighbourCount()
+    expect(cell.alive()).toBeFalse()
   });
 
 });
